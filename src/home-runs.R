758dc520e771650358a8b62e522dd104326c6e14
Ryan Elmore <rtelmore@gmail.com>
2018-10-08 14:16:33 -0600

    figures

--- src/home-runs.R
+++ src/home-runs.R
@@ -24,3 +24,35 @@ for(i in 2:(length(date_seq)-1)){
                     filter(events == "home_run"))
 }
 
+saveRDS(df, "data/home-runs.rds")
+
+## Bar Chart
+df_2 <- mutate(df, 
+             count = paste(balls, strikes, sep = "-")) %>%
+  group_by(count, stand, p_throws) %>%
+  summarize(n = n()) %>%
+  ungroup() %>%
+  arrange(n)
+
+p <- ggplot(data = group_by(df_2, count) %>%
+              summarize(nn = sum(n)) %>%
+              ungroup() %>%
+              arrange(desc(nn)),
+            aes(reorder(count, nn), nn))
+p + geom_col() +
+  labs(x = "count",
+       y = "number of home runs") +
+  theme_bw() +
+  coord_flip()
+ggsave("fig/home-runs-by-count.png")
+
+p <- ggplot(data = df_2,
+            aes(reorder(count, n), n, fill = p_throws))
+p + geom_col() +
+  facet_wrap(~ stand) +
+  scale_fill_brewer("pitcher", palette = "Dark2") +
+  labs(x = "count",
+       y = "number of home runs") +
+  coord_flip() +
+  theme_bw()
+ggsave("fig/home-runs-by-count-pitching.png")
